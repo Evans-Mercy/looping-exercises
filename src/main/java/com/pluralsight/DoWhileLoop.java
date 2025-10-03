b7c8a5755bd67d580c73098f4b57f824c12a3b9b
Evans-Mercy <cheria.mercy@gmail.com>
2025-10-03 13:34:16 -0500

    Loops Step 1

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+
+        do{
+            count ++;
+            System.out.println("I love java");
+        } while (count <= 5);
+
+    }
+}
